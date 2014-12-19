440c117fc7ac2478655b1ac403cc793114518afb
9dev <9devmail@gmail.com>
2014-12-19 13:05:17 +0100

    Improved demo views

--- demo/main/views.py
+++ demo/main/views.py
@@ -1,10 +1,14 @@
 from django.views.generic import FormView
+from django.http import HttpResponse
 from ratelimit9.mixins import Ratelimit9Mixin
 from main.forms import MyForm
 
 class MyView(Ratelimit9Mixin, FormView):
-	template_name = 'form.html'
+	template_name = 'main/form.html'
 	form_class = MyForm
 	success_url = '/sent'
 	ratelimit_key = 'ip'
 	ratelimit_rate = '3/m'
+	
+	def form_valid(self, form):
+		return HttpResponse('Form has been successfully sent.')
