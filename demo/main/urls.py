0e0049245aff894daf5cd005e0e88accaf7a1a26
9dev <9devmail@gmail.com>
2014-12-19 12:48:13 +0100

    Fixed urls in demo

--- demo/main/urls.py
+++ demo/main/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import patterns, url
 from main import views
 
 urlpatterns = patterns('',
-	url(r'^$', views.index, name='index'),
+	url(r'^$', views.MyView.as_view(), name='index'),
 )
 
